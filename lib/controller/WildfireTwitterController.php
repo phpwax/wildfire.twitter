2a7c0822d177a04c42eba31cf41386d127d02957
charles marshall <charles@oneblackbear.com>
2012-01-26 16:37:04 +0000

    custom number of tweets by slicing the array

--- lib/controller/WildfireTwitterController.php
+++ lib/controller/WildfireTwitterController.php
@@ -1,9 +1,11 @@
 <?
 class WildfireTwitterController extends WaxController{
+    
+    public $number_of_tweets = 1;
 
     public function __twitter(){
       $twitter = new WaxBackgroundCurl(array("url"=>"http://twitter.oneblackbear.com/".Config::get("twitter/username")."/statuses.sphp"));
-      $this->tweets = unserialize($twitter->fetch());
+      $this->tweets = array_slice((array)unserialize($twitter->fetch()), 0, $this->number_of_tweets);
     }
 
 }
